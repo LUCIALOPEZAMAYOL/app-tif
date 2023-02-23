660150e9519c23da3b726338bc44f430e33d460f
Lucia <lopezamayol@gmail.com>
2023-02-23 11:50:44 -0500

    integrando proyecto con git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { Error404Component } from './components/error404/error404.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    Error404Component,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
