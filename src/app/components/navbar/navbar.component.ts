b2aba42549ff41673bf3099d262cfc876b3eef9f
Lucia <llopeza@alumno.unsm.edu.pe>
2023-02-24 17:58:49 -0500

    Actualización de componente navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GlobalService } from 'src/app/shared/services/global.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  statesLogin = false
+  constructor(private globalService: GlobalService){
+    this.changeStateLogin()
+  }
+
+  changeStateLogin(){
+    this.statesLogin = this.globalService.statesLogin
+  }
 
 }
