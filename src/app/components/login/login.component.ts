7cabf34945fc0b64215ea10a9485229c93516e95
Lucia <llopeza@alumno.unsm.edu.pe>
2023-02-24 11:23:58 -0500

    add template login y create formLogin

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  formLogin = this.formBuilder.group({
+    email: ['', [Validators.email, Validators.required]],
+    password: ['', [Validators.required]]
+  })
+
+  constructor(
+    private formBuilder: FormBuilder
+  ){
+
+  }
+
+  login(){
+    console.log(this.formLogin.value.email);
+    this.formLogin.reset()
+  }
+
+  clean(){
+    this.formLogin.reset()
+  }
 }
